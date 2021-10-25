3552049fa4ace40a9e9d16a39cc34a967187062a
sldless <67599596+sldless@users.noreply.github.com>
2021-10-25 17:05:20 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from random import *
 import time
 turtle = Turtle()
 screen = Screen()
+enemy = Turtle()
 turtle.shape("triangle")
 hideturtle()
 turtle.color("white")
@@ -15,8 +16,13 @@ food.shape("turtle")
 food.color("green")
 food.penup()
 food.goto(20,100)
+enemy.shape('turtle')
+enemy.speed(0)
+enemy.goto(0, 60)
+enemy.penup()
+enemy.color('red')
+enemy.clear()
 score = 0
-highscore = 0
 def up():
     if turtle.go != "down":
         turtle.sety(turtle.ycor() + 20)
@@ -26,11 +32,11 @@ def down():
         turtle.sety(turtle.ycor() - 20)
 
 def left():
-    if turtle.go != "right":
+    if turtle.go != "left":
         turtle.setx(turtle.ycor() - 20)
 
 def right():
-    if turtle.go != "left":
+    if turtle.go != "right":
         turtle.setx(turtle.ycor() + 20)
 pen = Turtle()
 pen.speed(0)
@@ -39,34 +45,44 @@ pen.penup()
 pen.hideturtle()
 pen.goto(0, 100)
 turtle.clear()
-pen_high = Turtle()
-pen_high.speed(0)
-pen_high.color("white")
-pen_high.penup()
-pen_high.hideturtle()
-pen_high.goto(0, 60)
-pen_high.write("HighScore: 0", align="center", font=("Courier", 24, "normal"))
-turtle.clear()
 pen.write("Score: 0", align="center", font=("Courier", 24, "normal"))
+end = Turtle()
+end.speed(0)
+end.color('black')
+end.penup()
+end.hideturtle()
+end.goto(0, 100)
+end.clear()
 color(randint(0, 255), randint(0, 255),randint(0, 255))
+screen.bgcolor('black')
 screen.listen()
 screen.onkey(up, 'Up')
 screen.onkey(down, 'Down')
 screen.onkey(left, 'Left')
 screen.onkey(right, 'Right')
-screen.bgcolor('black')
+screen.onkey(up, 'w')
+screen.onkey(down, 's')
+screen.onkey(left, 'a')
+screen.onkey(right, 'd')
 while True:
     screen.update()
-    x = randint(-130, 130)
-    y = randint(-30, 20)
-    food.goto(x,y)
-    if score > highscore:
-        highscore = score+1
-    if turtle.distance(food) <= 20:    
+    fx = randint(-130, round(turtle.distance(food)))
+    fy = randint(-round(enemy.distance(turtle)), 20)
+    ex = randint(-round(enemy.distance(turtle)), 190)
+    ey = randint(-130, round(turtle.distance(food)))
+    food.goto(fx,fy)
+    enemy.goto(ex,ey)
+    print(turtle.distance(food), enemy.distance(turtle))
+    if turtle.distance(food) < 20:    
         score += 1      
         pen.clear()
-        pen_high.clear()
         pen.write('Score: {}'.format(score), align="center", font=("Courier", 24, "normal"))
-        pen.write("HighScore: {}".format(highscore), align="center", font=("Courier", 24, "normal"))
+    if enemy.distance(turtle) < 20:   
+        pen.clear()
+        pen.write('Game ended', align="center", font=("Courier", 24, "normal"))
+        time.sleep(0.80)
+        pen.clear()
+        pen.write('Score: 0', align="center", font=("Courier", 24, "normal"))
+    
     time.sleep(1.4)
 done()
